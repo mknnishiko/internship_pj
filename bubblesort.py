bd2b587f5f8161dd6f78cc9d069cb7ad41af4b1b
mknnishiko <75335392+mknnishiko@users.noreply.github.com>
2022-08-30 11:44:22 +0900

    [fix] bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,13 +1,13 @@
 array = [4, 10, 5, 8, 1, 2, 3, 9, 6, 7]
 
 for i in range(len(array) - 1):
-    for i in range(len(array) - 1):
-        num1 = array[i]
-        num2 = array[i + 1]
+    for j in range(len(array) - 1):
+        num1 = array[j]
+        num2 = array[j + 1]
         if num1 < num2:
             continue
         else:
-            array[i] = num2
-            array[i + 1] = num1
+            array[j] = num2
+            array[j + 1] = num1
 
 print(array)
